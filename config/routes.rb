2342fe0fff0ddcda3c13ac402ebdd5554f100676
mitsu-yama86 <y33ym22m@yahoo.co.jp>
2020-12-24 15:16:35 +0900

    商品詳細表示機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root  "items#index"
-  resources :items, only: [:new, :create, :destroy, :index]
+  resources :items, only: [:new, :create, :show, :destroy, :index]
 end
